96c0940c7e277be7cbd2e7adfd7e9eeaefb920d1
attiliogiglio <attilio.g93@gmail.com>
2020-09-10 19:36:50 -0300

    styled components emotion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './Components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      
-
-    </div>
+   <Header 
+   titulo='Cotizador de seguros'
+   />
   );
 }
 
